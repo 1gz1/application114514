96dc7d97ebf349044ce28e9668e0efd7cc739f49
1gz1 <luokaixin2015hahaha@outlook.com>
2025-10-12 09:34:03 +0800

    Update const.py

--- SourceCode/Source/const.py
+++ SourceCode/Source/const.py
@@ -1,4 +1,18 @@
-# 常量定义
-trange_n = 10000000
-userName = None
-passWord = None
+import json
+import os
+
+# 获取当前脚本所在的目录
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# 构造 JSON 文件的完整路径
+json_path = os.path.join(current_dir, 'const.json')
+
+# 打开并读取 JSON 文件
+try:
+    with open(json_path, 'r', encoding='utf-8') as file:
+        const = json.load(file)
+
+except FileNotFoundError:
+    print(f"文件 {json_path} 未找到。请检查文件路径是否正确，并确保文件存在。")
+except json.JSONDecodeError:
+    print(f"文件 {json_path} 解析错误。请检查文件内容是否为有效的 JSON 格式。")
